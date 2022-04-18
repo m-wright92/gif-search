50a7f626a3c1c5aed1541e0cd68fe6460d6fe510
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-18 13:49:24 -0700

    WIP: working on script to display trending gifs
    Co-authored-by: Mike Wright <m.wright92@icloud.com>
    Co-authored-by: Mark McConnell <amark.mcconnell@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,38 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$('#search-button').click(function() {
+  
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&rating=pg&lang=en`;
+  
+  let response;
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      response = JSON.parse(this.responseText);
+      getElements(response);
+      console.log(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  
+  
+
+  function getElements(response) {
+    let gifs = response.data;
+    let gifList = "<ul>";
+    console.log(gifs);
+    gifs.forEach(function (gif) {
+    let src = gif.url;
+    gifList.concat("<li>" + "<img src='" + src + "class='listed-gif' />" + "</li>");
+    console.log(gifList);
+  });
+
+  $('.gif-display').html(gifList + "</ul>");
+  }
+});
